73d1607ebb26e4fecb2de18f7740a5897b25ae8e
Verena Lechner <v.m.s.lechner@gmail.com>
2025-06-26 22:18:04 +0200

    referted adding repositoryconfig to not access DB when in keepalive profile

--- src/main/java/com/example/shop/config/RepositoryConfig.java
+++ src/main/java/com/example/shop/config/RepositoryConfig.java
@@ -1,12 +1,12 @@
-package com.example.shop.config;
+// package com.example.shop.config;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
-@Profile("!keepalive")
-@EnableJpaRepositories(basePackages = "com.example.shop.repository")
-public class RepositoryConfig {
-}
+// @Configuration
+// @Profile("!keepalive")
+// @EnableJpaRepositories(basePackages = "com.example.shop.repository")
+// public class RepositoryConfig {
+// }
